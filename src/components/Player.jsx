5c47114000eed500ed3e9ff9399d6e8f2d79b8a6
Abdallah Ragab <a.ragab8882@gmail.com>
2023-12-05 23:23:51 +0200

    Static GameBoard

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
   const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
+    //
     setIsEditing((editing) => !editing);
   }
 
   function handleChange(event) {
     setPlayerName(event.target.value);
+    console.log(playerName);
+    
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
@@ -25,10 +28,12 @@ export default function Player({ initialName, symbol }) {
   return (
     <li>
       <span className="player">
-        {editablePlayerName}
+     {/* { !isEditing && <span className="player-name">{playerName}</span> }
+     { isEditing && <input type="text" required value={playerName} onChange={handleChange} /> } */}
+     {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
